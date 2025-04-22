27be7f996b41c8d276b9cbda11955b3bfc4cc36f
reachnaveen <reachnaveen@gmail.com>
2025-04-22 11:43:39 -0700

    Update HelloController.java

--- app/src/main/java/com/example/springboot/HelloController.java
+++ app/src/main/java/com/example/springboot/HelloController.java
@@ -2,6 +2,7 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class HelloController {
