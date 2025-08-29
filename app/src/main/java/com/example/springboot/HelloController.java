7a8f5737a765db989bc872e6db99442da1981054
reachnaveen <reachnaveen@gmail.com>
2025-08-29 09:44:54 -0700

    Update HelloController.java

--- app/src/main/java/com/example/springboot/HelloController.java
+++ app/src/main/java/com/example/springboot/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Hello Blue-Green ... " + greeterMessageFormat;
+		return "Hello Blue-Green Test... " + greeterMessageFormat;
 	}
 
 }
